63bbd563f79a7013406c9cd7c029c36a19538066
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-10 13:56:23 +0300

    changed path to admin

--- src/main/java/com/example/FoodDeliveryBackend/infrastructure/web/controller/RestaurantAdminController.java
+++ src/main/java/com/example/FoodDeliveryBackend/infrastructure/web/controller/RestaurantAdminController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/restaurant-admin")
+@RequestMapping("/api/v1/admin")
 public class RestaurantAdminController {
 
     @PostMapping("/restaurant")
