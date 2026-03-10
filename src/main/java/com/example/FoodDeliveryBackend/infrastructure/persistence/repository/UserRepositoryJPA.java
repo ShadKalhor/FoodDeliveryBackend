4f71210666a3c24cbca32f9f7df360f8c1c133c0
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-10 14:07:53 +0300

    added findByEmail and existsByEmail

--- src/main/java/com/example/FoodDeliveryBackend/infrastructure/persistence/repository/UserRepositoryJPA.java
+++ src/main/java/com/example/FoodDeliveryBackend/infrastructure/persistence/repository/UserRepositoryJPA.java
@@ -1,10 +1,14 @@
 package com.example.FoodDeliveryBackend.infrastructure.persistence.repository;
 
 import com.example.FoodDeliveryBackend.infrastructure.persistence.entity.User;
+import io.vavr.control.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface UserRepositoryJPA extends JpaRepository<User, UUID> {
 
+    Option<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
