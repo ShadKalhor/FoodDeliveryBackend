73424209b6c4afb3ec150e7a86e07245f4a5376e
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-12 11:48:48 +0300

    Added KeycloakRoleConverter.java and made SecurityConfig.java implement it

---
+++ src/main/java/com/example/FoodDeliveryBackend/infrastructure/security/KeycloakRoleConverter.java
@@ -0,0 +1,36 @@
+package com.example.FoodDeliveryBackend.infrastructure.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+
+        if (realmAccess == null || realmAccess.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        Object rolesObject = realmAccess.get("roles");
+        if (!(rolesObject instanceof List<?> roles)) {
+            return Collections.emptyList();
+        }
+
+        return roles.stream()
+                .filter(String.class::isInstance)
+                .map(String.class::cast)
+                .map(role -> "ROLE_" + role)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
