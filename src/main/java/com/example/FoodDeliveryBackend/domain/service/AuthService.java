532dcc8fb35f4360e48e8804f1c695c3d23b29de
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-12 11:46:41 +0300

    .

--- src/main/java/com/example/FoodDeliveryBackend/domain/service/AuthService.java
+++ src/main/java/com/example/FoodDeliveryBackend/domain/service/AuthService.java
@@ -5,7 +5,5 @@ import com.example.FoodDeliveryBackend.domain.port.out.UserRepositoryPort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AuthService {
-    private UserRepositoryPort userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
+
 }
