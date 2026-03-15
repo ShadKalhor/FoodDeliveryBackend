e8eedd99fc3b36cd44ee4129cacd98a05d1e6184
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-15 11:48:19 +0300

    removed preautherize

--- src/main/java/com/example/FoodDeliveryBackend/infrastructure/web/controller/CustomerController.java
+++ src/main/java/com/example/FoodDeliveryBackend/infrastructure/web/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.example.FoodDeliveryBackend.infrastructure.web.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
