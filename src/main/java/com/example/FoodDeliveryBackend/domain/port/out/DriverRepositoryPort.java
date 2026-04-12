6e6ce2e0a4e0c957144d55a2463fbdaa17afb2ed
shadKalhor <shad.jalal.aziz@gmail.com>
2026-04-12 10:36:16 +0300

    Added A Port Interface With The Following Database Operations - save - findById - findAll - deleteById

---
+++ src/main/java/com/example/FoodDeliveryBackend/domain/port/out/DriverRepositoryPort.java
@@ -0,0 +1,23 @@
+package com.example.FoodDeliveryBackend.domain.port.out;
+
+import com.example.FoodDeliveryBackend.domain.exception.StructuredError;
+import com.example.FoodDeliveryBackend.domain.model.DriverDomain;
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface DriverRepositoryPort {
+
+    Either<StructuredError, DriverDomain> save(DriverDomain driverDomain);
+
+    Option<DriverDomain> findById(UUID driverId);
+
+    List<DriverDomain> findAll();
+
+    Either<StructuredError, Void> deleteById(UUID driverId);
+
+
+
+}
