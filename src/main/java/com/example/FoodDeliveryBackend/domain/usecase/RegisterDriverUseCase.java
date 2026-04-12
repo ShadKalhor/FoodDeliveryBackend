b0454a1a958b5d7b3504be6b249a9e0f1561b04c
shadKalhor <shad.jalal.aziz@gmail.com>
2026-04-12 10:37:12 +0300

    Register a New Driver UseCase That Creates a New Account and Registers The Account As a Driver

---
+++ src/main/java/com/example/FoodDeliveryBackend/domain/usecase/RegisterDriverUseCase.java
@@ -0,0 +1,74 @@
+package com.example.FoodDeliveryBackend.domain.usecase;
+
+import com.example.FoodDeliveryBackend.domain.accountCommands.AddAccountCommand;
+import com.example.FoodDeliveryBackend.domain.driverCommands.AddDriverCommand;
+import com.example.FoodDeliveryBackend.domain.enums.Roles;
+import com.example.FoodDeliveryBackend.domain.enums.VehicleType;
+import com.example.FoodDeliveryBackend.domain.exception.StructuredError;
+import io.vavr.control.Either;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class RegisterDriverUseCase {
+
+    private final AddAccountCommand addAccountCommand;
+    private final AddDriverCommand addDriverCommand;
+
+    public Either<StructuredError, RegisterDriverOutput> execute(RegisterDriverInput input) {
+        return addAccountCommand.execute(
+                        new AddAccountCommand.SaveAccountInput(
+                                input.getName(),
+                                input.getPhone(),
+                                input.getEmail(),
+                                input.getPassword(),
+                                Roles.DRIVER
+                        )
+                )
+                .flatMap(accountOutput ->
+                        addDriverCommand.execute(
+                                new AddDriverCommand.SaveDriverInput(
+                                        accountOutput.getId(),
+                                        input.getVehicleType()
+                                )
+                        ).map(driverOutput ->
+                                RegisterDriverOutput.of(accountOutput, driverOutput)
+                        )
+                );
+    }
+
+    @Value
+    public static class RegisterDriverInput {
+        String name;
+        String phone;
+        String email;
+        String password;
+        VehicleType vehicleType;
+    }
+
+    @Value
+    public static class RegisterDriverOutput {
+        UUID accountId;
+        UUID driverId;
+        String name;
+        String email;
+        String phone;
+        VehicleType vehicleType;
+
+        public static RegisterDriverOutput of(
+                AddAccountCommand.SaveAccountOutput account,
+                AddDriverCommand.SaveDriverOutput driver
+        ) {
+            return new RegisterDriverOutput(
+                    account.getId(),
+                    driver.getId(),
+                    account.getName(),
+                    account.getEmail(),
+                    account.getPhone(),
+                    driver.getVehicleType()
+            );
+        }
+    }
+}
