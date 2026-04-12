c78060d6ea52b5132402bf77990c4edbba994111
shadKalhor <shad.jalal.aziz@gmail.com>
2026-04-12 10:26:13 +0300

    Command For Adding Driver to The Database.

---
+++ src/main/java/com/example/FoodDeliveryBackend/domain/driverCommands/AddDriverCommand.java
@@ -0,0 +1,57 @@
+package com.example.FoodDeliveryBackend.domain.driverCommands;
+
+import com.example.FoodDeliveryBackend.domain.enums.VehicleType;
+import com.example.FoodDeliveryBackend.domain.exception.StructuredError;
+import com.example.FoodDeliveryBackend.domain.model.DriverDomain;
+import com.example.FoodDeliveryBackend.domain.port.out.DriverRepositoryPort;
+import io.vavr.control.Either;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class AddDriverCommand {
+
+    private final DriverRepositoryPort driverRepositoryPort;
+
+
+    public Either<StructuredError, SaveDriverOutput> execute(SaveDriverInput input){
+        return driverRepositoryPort.save(input.toParams()).map(SaveDriverOutput::of);
+    }
+
+
+    @Value
+    public static class SaveDriverInput{
+
+        UUID accountId;
+        VehicleType vehicleType;
+
+        private DriverDomain toParams() {
+            return new DriverDomain(UUID.randomUUID(), accountId,vehicleType,
+                    true,null,
+                    null,BigDecimal.ZERO);
+        }
+
+    }
+
+    @Value
+    public static class SaveDriverOutput{
+
+        UUID id;
+        UUID accountId;
+        VehicleType vehicleType;
+        BigDecimal avgRating;
+
+        static SaveDriverOutput of(DriverDomain domain) {
+            return new SaveDriverOutput(domain.getId(), domain.getUserId(),
+                    domain.getVehicleType(), domain.getAvgRating());
+        }
+
+
+    }
+
+}
+
+
