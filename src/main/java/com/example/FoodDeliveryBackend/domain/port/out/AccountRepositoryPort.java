eb5cc6880382f1327cd6da6e420d8410f1bf08e5
shadKalhor <shad.jalal.aziz@gmail.com>
2026-04-12 10:34:22 +0300

    Added A Port Interface With The Following Database Operations - save - findById - findByName - findAll - findByPhone - deleteById

--- src/main/java/com/example/FoodDeliveryBackend/domain/port/out/AccountRepositoryPort.java
+++ src/main/java/com/example/FoodDeliveryBackend/domain/port/out/AccountRepositoryPort.java
@@ -1,6 +1,27 @@
 package com.example.FoodDeliveryBackend.domain.port.out;
 
+import com.example.FoodDeliveryBackend.domain.exception.StructuredError;
+import com.example.FoodDeliveryBackend.domain.model.AccountDomain;
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface AccountRepositoryPort {
+
+    Either<StructuredError, AccountDomain> save(AccountDomain domain);
+
+    Option<AccountDomain> findById(UUID accountId);
+
+    Option<AccountDomain> findByName(String name);
+
+    List<AccountDomain> findAll();
+
+    Option<AccountDomain> findByPhone(String phoneNumber);
+
+    Either<StructuredError, Void> deleteById(UUID userId);
+
 
 
 
